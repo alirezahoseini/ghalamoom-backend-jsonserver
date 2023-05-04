ad80f68e373b119f0fbbab930ac53cb50c2a27e7
Alirea Hosseini <mehdimj0161@gmail.com>
2023-05-04 18:16:51 +0430

    add json-server-auth to project

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+const middlewares = jsonServer.defaults();
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const cors = require('cors');
 
-server.use(middlewares)
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+server.options('*', cors());
+
+server.use(middlewares);
+server.db = router.db
+server.use(auth)
 server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+server.listen(5000, ()=>{
+  console.log('server is runing.!')
 })
